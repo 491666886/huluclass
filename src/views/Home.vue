848f6e0d28b0144bcb76d91e851294fb25082b16
hjy <491666886@qq.com>
2020-02-04 15:17:08 +0800

    测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="home">
-
     <div class="app">
-    <span>Multiline message is:</span>
-    <p>{{message}}</p>
-    <br>
-    <textarea name="" v-model="message" placeholder="please write..."></textarea>
-  </div>
+      <span>Multiline message is:</span>
+      <p>{{message}}</p>
+      <br />
+      <textarea name v-model="message" placeholder="please write..."></textarea>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'home',
-  components: {
-   
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
